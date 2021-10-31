08122f366d39315d2c6fbf54cb743706027d7f06
Pedro Pacheco <pacheckdev@gmail.com>
2021-10-31 07:54:54 -0300

    updated signin component

--- src/views/SignInFlow/SignIn.vue
+++ src/views/SignInFlow/SignIn.vue
@@ -3,13 +3,7 @@
     class="container"
     :class="{ 'light-background': !isDarkMode, 'dark-background': isDarkMode }"
   >
-    <div
-      class="request"
-      :class="{ 'light-request': isDarkMode, 'dark-request': !isDarkMode }"
-    >
-      Don't have a Design+Code HQ account?
-      <router-link to="/request">Request an account</router-link>
-    </div>
+    <RequestAccount />
     <div class="login">
       <img src="@/assets/DCHQ.png" alt="" v-show="isDarkMode" />
       <img src="@/assets/DCHQ-dark.png" alt="" v-show="!isDarkMode" />
@@ -40,49 +34,31 @@
         :class="{ 'light-link': isDarkMode, 'dark-link': !isDarkMode }"
         >Forgot your password?</router-link
       >
-      <button @click="toggleDarkMode">Toggle</button>
+      <ThemeSwitch />
     </div>
   </div>
 </template>
 
 <script>
+import RequestAccount from "@/components/RequestAccount.vue";
+import ThemeSwitch from "@/components/ThemeSwitch.vue";
+
 export default {
   name: "SignIn",
-  data() {
-    return {
-      isDarkMode: true,
-    };
+  components: {
+    RequestAccount,
+    ThemeSwitch,
   },
-  methods: {
-    toggleDarkMode() {
-      this.isDarkMode = !this.isDarkMode;
-      document.body.style.background = this.isDarkMode ? "#212c4f" : "#f0f3f5";
+
+  computed: {
+    isDarkMode() {
+      return this.$store.state.isDarkMode;
     },
   },
 };
 </script>
 
 <style lang="scss" scoped>
-.light-background {
-  background-color: $light-gray;
-}
-.dark-background {
-  background-color: $dark-blue;
-}
-.light-text {
-  color: $white;
-}
-.dark-text {
-  color: $black;
-}
-.light-field {
-  background-color: rgba(255, 255, 255, 0.2);
-  border: 1px solid rgba(255, 255, 255, 0.2);
-
-  &::placeholder {
-    color: rgba(255, 255, 255, 0.3);
-  }
-}
 .dark-field {
   background-color: rgba(198, 208, 235, 0.2);
   border: 1px solid rgba(0, 0, 0, 0.2);
@@ -101,82 +77,5 @@ export default {
 
 .login {
   width: 400px;
-}
-
-.request {
-  position: absolute;
-  top: 40px;
-  right: 40px;
-  color: rgba(255, 255, 255, 0.3);
-  a {
-    color: #fff;
-  }
-}
-.light-request {
-  color: rgba(255, 255, 255, 0.3);
-
-  a {
-    color: $white;
-  }
-}
-.dark-request {
-  color: rgba(0, 0, 0, 0.3);
-
-  a {
-    color: $black;
-  }
-}
-
-h4 {
-  margin: 0;
-  font-size: 24px;
-  text-align: center;
-  color: #fff;
-}
-
-input {
-  width: 100%;
-  height: 60px;
-  background: rgba(255, 255, 255, 0.2);
-  border: 1px solid rgba(255, 255, 255, 0.2);
-  box-sizing: border-box;
-  border-radius: 4px;
-
-  font-size: 20px;
-  color: #fff;
-  padding-left: 20px;
-  margin-top: 20px;
-
-  &::placeholder {
-    color: rgba(255, 255, 255, 0.3);
-  }
-}
-
-button {
-  height: 60px;
-  width: 100%;
-  font-size: 20px;
-  border: none;
-  color: #fff;
-  margin-top: 20px;
-  background: #56ccf2;
-  box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.2);
-  border-radius: 4px;
-  margin-bottom: 40px;
-}
-
-a {
-  text-decoration: none;
-  line-height: 25px;
-  /* identical to box height */
-
-  text-align: center;
-}
-
-.light-link {
-  color: rgba(255, 255, 255, 0.3);
-}
-.dark-link {
-  color: rgba(0, 0, 0, 0.3);
 }
 </style>
